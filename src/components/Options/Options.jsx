ec5abb878d845cbe2116cf6cda6d8d35078c9604
ValkinDED <sonyerik289@gmail.com>
2025-03-10 16:47:24 +0100

    Initial commit

---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,12 @@
+import css from "./Options.module.css";
+
+const Options = ({ updateFeedback, resetFeedback, totalFeedback }) => (
+  <div className={css.options}>
+    <button onClick={() => updateFeedback("good")}>Good</button>
+    <button onClick={() => updateFeedback("neutral")}>Neutral</button>
+    <button onClick={() => updateFeedback("bad")}>Bad</button>
+    {totalFeedback > 0 && <button onClick={resetFeedback}>Reset</button>}
+  </div>
+);
+
+export default Options;
